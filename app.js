b9d409d6a4919bb9e1aa69810df793094c68d45f
cody-chef <96326316+cody-chef@users.noreply.github.com>
2022-01-18 21:24:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+function calculateWater() {
+  var weight = document.getElementById("weight").value;
+  var activity = document.getElementById("exercise").value;
+  var requiredWaterOz = weight / 2 + (activity / 30) * 12;
+  var waterBottles = requiredWaterOz / 16.9;
+  var waterBottles2 = waterBottles.toFixed(2);
+  if (isNaN(weight) || isNaN(activity)) {
+    alert("Please enter only numeric values.");
+  }
+  else if (weight === "") {
+    alert("Please enter your weight.");
+  }
+  else if (activity === "") {
+    alert("Please enter your exercise time. If you have not been active, please enter 0.");
+  }
+  else {
+    document.getElementById("required_water").value = requiredWaterOz;
+    document.getElementById("water_bottles").value = waterBottles2;
+  }
+}
+function resetForm() {
+	document.getElementById("personal_info").reset();
+}
+
+
+
 const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
@@ -28,8 +54,8 @@ smallCups.forEach((cup, idx) => {
             }, 1000);
         }
         startInterval();
-        h.value = 2;
-        m.value = 0;
+        h.value = 0;
+        m.value = 1;
         s.value = 0;
         highlightCups(idx)
     })
